1cd8a9629e0446b8f280ff6936e400bbbc7333b4
Marcaus.Cruz <marcauscruz@gmail.com>
2025-06-09 13:27:48 -0700

    Scaffolding for hitlist?

--- src/app/hitlist/page.tsx
+++ src/app/hitlist/page.tsx
@@ -3,4 +3,21 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase("http://127.0.0.1:8090"); // TODO: Make this an exportable const
 
-export default function Hitlist() {}
+export default function Hitlist() {
+  const ownerHitlist = pb.authStore.record?.hitlist ?? [];
+  console.log(`[Hitlist]`, { ownerHitlist });
+
+  return (
+    <div>
+      <h1>Hitlist</h1>
+      <ul>
+        {ownerHitlist.map((placeToHit) => (
+          <li key={placeToHit.id}>{placeToHit.restName}</li>
+        ))}
+      </ul>
+      <button className="btn add" onClick={() => {}}>
+        Add to hitlist
+      </button>
+    </div>
+  );
+}
