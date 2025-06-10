7004d1ce15db9c80ed0be6f6f0c02bf5db38d79b
Marcaus.Cruz <marcauscruz@gmail.com>
2025-06-10 16:09:57 -0700

    Create common pb calls

--- src/app/lib/pocketbase.ts
+++ src/app/lib/pocketbase.ts
@@ -1,5 +1,22 @@
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
+import type { Hitlist } from "../types/hitlist.types";
 
-const client = new PocketBase('http://127.0.0.1:8090');
+const client = new PocketBase("http://127.0.0.1:8090");
+
+export const COLLECTION_USER = client.collection("users");
+
+export const getLocalId = (): string => {
+  return client.authStore.record?.id ?? "";
+};
+
+export const getLocalHitlist = (): Hitlist => {
+  return client.authStore.record?.hitlist ?? [];
+};
+
+export const setLocalHitlist = (newHitlist: Hitlist): void => {
+  if (client.authStore.record) {
+    client.authStore.record.hitlist = newHitlist;
+  }
+};
 
 export default client;
