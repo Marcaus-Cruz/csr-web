6ff10b7f6bf3a9e7b4e5aaa5c6824e0569aa1b09
Marcaus.Cruz <marcauscruz@gmail.com>
2025-06-16 15:05:25 -0700

    Set heights on header and pages. Only set page bg on login pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
       <body>
         <PageHeader />
-        <div className="page-bg" />
         {children}
+        <div className="page-bg" />
       </body>
     </html>
   );
